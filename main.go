8d312e993901f355540d134b018e447e736d2bd2
Miguel Pedrozo <miguel.pedrozo@avaldigitallabs.com>
2020-04-23 15:08:46 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mpedrozoduran/gogrpctest/timemanager"
+	pb "github.com/mpedrozoduran/gogrpctest/timeproto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	PORT = 9091
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", PORT))
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterTimeManagerServer(grpcServer, &timemanager.TimeStruct{})
+	grpcServer.Serve(lis)
+}
